c035b3864c708d5d289e638713d0a40e5ce9f585
Trever Fischer <tdfischer@fedoraproject.org>
2012-10-19 17:06:38 -0400

    Draw up a basic front page for people who arent logged in

--- spiff/local/views.py
+++ spiff/local/views.py
@@ -2,5 +2,9 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 
 def index(request):
-  return render_to_response('local/index.html',
-      context_instance=RequestContext(request))
+  if request.user.is_anonymous():
+    return render_to_response('local/index_anonymous.html',
+        context_instance=RequestContext(request))
+  else:
+    return render_to_response('local/index.html',
+        context_instance=RequestContext(request))
