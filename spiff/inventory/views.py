8faabf85f3d1d2caa861f77f89ae661377fdcfcd
Trever Fischer <tdfischer@fedoraproject.org>
2012-10-21 11:23:34 -0400

    Add QR codes to inventory screens

--- spiff/inventory/views.py
+++ spiff/inventory/views.py
@@ -1,7 +1,13 @@
 from django.template import RequestContext
+from django.contrib.sites.models import get_current_site
+from django.core.urlresolvers import reverse
+from django.views.decorators.cache import cache_control
+from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import render_to_response
+from cStringIO import StringIO
 import models
+import qrcode
 
 def index(request):
   resources = models.Resource.objects.all()
@@ -23,3 +29,15 @@ def train(request, id):
   resource = models.Resource.objects.get(pk=id)
   return render_to_response('inventory/train.html', {'item':resource},
       context_instance=RequestContext(request))
+
+@cache_control(public=True, private=False, no_cache=False, no_transform=False, must_revalidate=False, proxy_revalidate=False, max_age=86400)
+def qrCode(request, id):
+  site = get_current_site(request)
+  base = "%s://%s"%(request.META['wsgi.url_scheme'], site.domain)
+  if request.META['SERVER_PORT']:
+    base = "%s:%s"%(base, request.META['SERVER_PORT'])
+  img = qrcode.make("%s%s"%(base,
+    reverse('spiff.inventory.views.view', kwargs={'id': id})))
+  buf = StringIO()
+  img.save(buf, "PNG")
+  return HttpResponse(buf.getvalue(), content_type="image/png")
