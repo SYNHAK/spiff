e0ea3fbcc6e190afc3e025cb246e6fbc71ab04af
Trever Fischer <tdfischer@fedoraproject.org>
2012-10-21 12:58:24 -0400

    Implement member profile editing

--- spiff/membership/urls.py
+++ spiff/membership/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('spiff.membership',
   url(r'^$', 'views.index'),
-  url(r'^(?P<username>.+)$', 'views.view'),
+  url(r'^view/(?P<username>.+)$', 'views.view'),
+  url(r'^edit/(?P<username>.+)$', 'views.edit'),
+  url(r'^edit$', 'views.edit'),
 )
--- spiff/membership/views.py
+++ spiff/membership/views.py
@@ -1,7 +1,9 @@
 from django.template import RequestContext
+from django.contrib import messages
 from django.contrib.auth.models import User
 from django.shortcuts import render_to_response
 import models
+import forms
 
 def index(request):
   users = User.objects.filter(is_active=True)
@@ -13,4 +15,35 @@ def view(request, username):
   user = User.objects.get(username=username)
   return render_to_response('membership/view.html',
       {'viewUser': user},
+      context_instance=RequestContext(request))
+
+def edit(request, username=None):
+  if username is None:
+    user = request.user
+  else:
+    user = User.objects.get(username=username)
+  fields = models.Field.objects.filter()
+  values = user.member.attributes.all()
+  if request.method == "POST":
+    userForm = forms.UserForm(request.POST, instance=user)
+    profileForm = forms.ProfileForm(request.POST, fields=fields, values=values)
+  else:
+    userForm = forms.UserForm(instance=user)
+    profileForm = forms.ProfileForm(fields=fields, values=values)
+  if userForm.is_valid() and profileForm.is_valid():
+    user.first_name = userForm.cleaned_data['firstName']
+    user.last_name = userForm.cleaned_data['lastName']
+    user.save()
+    member = user.member
+    member.birthday = userForm.cleaned_data['birthday']
+    member.profession = userForm.cleaned_data['profession']
+    member.save()
+    for f in fields:
+      value,created = models.FieldValue.objects.get_or_create(member=member, field=f)
+      value.value = profileForm.fieldValue(f)
+      value.save()
+    messages.info(request, "Profile Saved.")
+
+  return render_to_response('membership/edit.html',
+      {'editUser': user, 'userForm':userForm, 'profileForm':profileForm},
       context_instance=RequestContext(request))
