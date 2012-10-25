5f396978d208e9b8eae0602641ddac40ad6d4c7c
Trever Fischer <tdfischer@fedoraproject.org>
2012-10-25 15:32:29 -0400

    Improve search by searching metadata

--- spiff/local/views.py
+++ spiff/local/views.py
@@ -6,7 +6,7 @@ import forms
 from spiff.membership.forms import ProfileForm
 from spiff.membership.models import FieldValue, Field, Member
 from spiff.events.models import Event
-from spiff.inventory.models import Resource
+from spiff.inventory.models import Resource, Metadata
 from django.contrib import messages
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.models import User
@@ -70,15 +70,16 @@ def search(request):
     searchForm = forms.SearchForm()
   if searchForm.is_valid():
     resources = Resource.objects.filter(
-      Q(name__iregex='.*%s.*'%(searchForm.cleaned_data['query'])) |
-      Q(metadata__value__iregex='.*%s.*'%(searchForm.cleaned_data['query']))
+      name__iregex='.*%s.*'%(searchForm.cleaned_data['query'])
+    )
+    metadata = Metadata.objects.filter(
+        value__iregex='.*%s.*'%(searchForm.cleaned_data['query'])
     )
     members = Member.objects.filter(
       Q(user__username__iregex='.*%s.*'%(searchForm.cleaned_data['query'])) |
       Q(user__first_name__iregex='.*%s.*'%(searchForm.cleaned_data['query'])) |
-      Q(user__last_name__iregex='.*%s.*'%(searchForm.cleaned_data['query'])) |
-      Q(attributes__value__iregex='.*%s.*'%(searchForm.cleaned_data['query']))
+      Q(user__last_name__iregex='.*%s.*'%(searchForm.cleaned_data['query']))
     )
   return render_to_response('local/search.html',
-      {'resources': resources, 'members': members},
+      {'resources': resources, 'members': members, 'metadata': metadata},
       context_instance=RequestContext(request))
