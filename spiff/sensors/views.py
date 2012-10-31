b326bf7c8a939d0067484e8204fc1c16f157d44a
Trever Fischer <tdfischer@fedoraproject.org>
2012-10-31 07:57:16 -0400

    Be pamela-compatible out of the box

--- spiff/sensors/views.py
+++ spiff/sensors/views.py
@@ -14,7 +14,7 @@ def view(request, id):
   sensor = models.Sensor.objects.get(pk=id)
   if request.POST:
     models.SensorValue.objects.create(sensor=sensor,
-        value=request.POST['value'])
+        value=request.POST['data'])
   return render_to_response('sensors/view.html',
       {'sensor': sensor},
       context_instance=RequestContext(request))
