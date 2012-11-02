cf1730f228bd43ae38f71c7d8ba9c871aa428879
Trever Fischer <tdfischer@fedoraproject.org>
2012-11-02 13:01:06 -0400

    Implement webfinger better, depending on 0.2

--- spiff/local/webfinger.py
+++ spiff/local/webfinger.py
@@ -1,5 +1,15 @@
 from django.core.urlresolvers import reverse
+from django.contrib.sites.models import Site
+from django.contrib.auth.models import User
 import datetime
 
-def handle(acct, xrd):
+def handle(request, acct, xrd):
+  print acct.host
+  site = Site.objects.get(domain=acct.host)
+  base = "%s://%s"%(request.META['wsgi.url_scheme'], site.domain)
+  if request.META['SERVER_PORT'] != '80':
+    base = "%s:%s"%(base, request.META['SERVER_PORT'])
+  user = User.objects.get(username=acct.userinfo)
   xrd.expires = datetime.datetime.utcnow() + datetime.timedelta(0, 10)
+  xrd.aliases.append("%s%s"%(base, reverse('spiff.membership.views.view',
+    kwargs={'username':user.username})))
