2cfc936419abf98426c10d3efa798ee12d059f9d
Trever Fischer <tdfischer@fedoraproject.org>
2012-11-01 21:49:26 -0400

    Add a setup.py so readthedocs.org can install into a virtualenv

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+setup(name='Spiff',
+    version='0.1',
+    description="Spaceman Spiff Manages Spaces",
+    author='Trever Fischer',
+    author_email='wm161@wm161.net',
+    url='http://github.com/synhak/spiff',
+    packages=['spiff']
+)
