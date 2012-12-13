5c1ed574db6701b2fac4b33e79b3d5edac50e09d
Trever Fischer <tdfischer@fedoraproject.org>
2012-12-13 18:14:17 -0500

    Fix Member permalinks

--- spiff/membership/models.py
+++ spiff/membership/models.py
@@ -52,7 +52,7 @@ class Member(models.Model):
 
   @models.permalink
   def get_absolute_url(self):
-    return ('membership:view', [], {'username': self.user.username})
+    return ('membership:view', [], {'user__username': self.user.username})
 
   @property
   def fullName(self):
