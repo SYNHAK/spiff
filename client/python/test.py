f6c7752c15c8c3d76e95024bbc3f2f4ac58d553f
Trever Fischer <wm161@wm161.net>
2013-05-01 11:05:27 -0400

    client: python: crash-- in test.py

--- client/python/test.py
+++ client/python/test.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 import spiff
+import logging
+logging.basicConfig(level=logging.DEBUG)
 
 api = spiff.API("https://synhak.org/auth/", verify=False)
 
 for res in api.resources():
     print type(res)
-    print res.type()
+    print res.type
