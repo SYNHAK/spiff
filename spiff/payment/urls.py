00e3873e44cc5f577dbfce26d7778464ed0e2412
Torrie Fischer <tdfischer@hackerbots.net>
2014-01-13 23:01:00 -0500

    Remove the now obsolete urls and views from payment app

--- spiff/payment/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, include, url
-
-urlpatterns = patterns('spiff.payment',
-  url(r'^pay/(?P<invoiceID>.+)$', 'views.pay', name='pay'),
-  url(r'^addPayment/(?P<invoiceID>.+)$', 'views.addPayment', name='addPayment'),
-  url(r'^invoice/(?P<invoiceID>.+)$', 'views.viewInvoice', name='viewInvoice'),
-)
