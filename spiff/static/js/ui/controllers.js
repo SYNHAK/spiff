a491eb04f9b7447c20b93b3600709b558848b2ee
Torrie Fischer <tdfischer@hackerbots.net>
2014-01-23 21:24:46 -0500

    Start working on modifying groups

--- spiff/static/js/ui/controllers.js
+++ spiff/static/js/ui/controllers.js
@@ -86,7 +86,40 @@ spiffControllers.controller('MemberListCtrl', function($scope, Restangular) {
 
 spiffControllers.controller('MemberCtrl', function($scope, Restangular, $routeParams) {
   var member = Restangular.one('member', $routeParams.memberID);
-  $scope.member = member.get().$object;
+  member.get().then(function (member) {
+    $scope.member = member;
+    console.log(member.groups);
+    Restangular.all('group').getList().then(function (groups) {
+      $scope.availableGroups = groups;
+      _.each(groups, function(group) {
+        if (_.find(member.groups, function(g) {return g.id == group.id;})) {
+          group.active = true;
+        } else {
+          group.active = false;
+        }
+      });
+    });
+  });
+
+  $scope.saveGroups = function() {
+    var newGroups = [];
+    $('#groups-form input[type="checkbox"]:checked').each(function(idx, c) {
+      newGroups.push("/v1/group/"+$(c).val()+"/");
+    });
+    console.log(newGroups);
+
+    member.patch({'groups': newGroups}).then(function() {
+      $('#modifyGroupsModal').modal('hide');
+    });
+  }
+
+  $scope.showModifyGroups = function() {
+    $('#modifyGroupsModal').modal('show');
+  }
+
+  $scope.hideModifyGroups = function() {
+    $('#modifyGroupsModal').modal('hide');
+  }
 });
 
 spiffControllers.controller('ResourceListCtrl', function($scope, Restangular) {
