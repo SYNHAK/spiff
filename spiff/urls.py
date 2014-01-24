1052713c2a85803254bc238f659d44e3f25ae153
Torrie Fischer <tdfischer@hackerbots.net>
2014-01-23 21:25:02 -0500

    Cleanup dead urls and views

--- spiff/urls.py
+++ spiff/urls.py
@@ -12,20 +12,14 @@ urlpatterns = patterns('',
     url(r'^$', RedirectView.as_view(url=reverse_lazy('ui:index')), name='root'),
     url(r'^ui/', include('spiff.ui.urls', namespace='ui')),
     url(r'^', include(v1_api.urls)),
-    #$url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'local/login.html'}),
-    #$url(r'^members/', include('spiff.membership.urls', namespace='membership')),
+
     #$url(r'^sensors/', include('spiff.sensors.urls', namespace='sensors')),
-    #$url(r'^resources/', include('spiff.inventory.urls', namespace='inventory')),
     #$url(r'^events/', include('spiff.events.urls', namespace='events')),
-    #$url(r'^accounts/', include('django.contrib.auth.urls')),
     #$url(r'^manage/', include('spiff.management.urls', namespace='management')),
     #$url(r'^openid_provider/', include('openid_provider.urls')),
     #$url(r'^openid/untrust/(?P<id>.*)', 'spiff.local.views.untrust_openid_root'),
     #$url(r'^openid/unassociate/(?P<id>.*)', 'spiff.local.views.unassociate_openid'),
     #$url(r'^openid/', include('django_openid_auth.urls')),
-    #$# Examples:
-    #$# url(r'^$', 'spiff.views.home', name='home'),
-    #$# url(r'^spiff/', include('spiff.foo.urls')),
 
     #$# Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
