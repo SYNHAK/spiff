d4cd07528f8f29696d188c528937698859929773
Torrie Fischer <tdfischer@hackerbots.net>
2014-01-26 21:20:32 -0500

    Fix invoice batching

--- spiff/payment/management/commands/process_subscriptions.py
+++ spiff/payment/management/commands/process_subscriptions.py
@@ -9,9 +9,9 @@ class Command(BaseCommand):
 
   def handle(self, *args, **options):
     startOfMonth, endOfMonth = monthRange()
+    lineItems = {}
     for planCls in find_api_classes('models', SubscriptionPlan):
       plans = planCls.objects.all()
-      lineItems = {}
       for plan in plans:
         for subscription in plan.subscriptions.filter(active=True):
           if subscription.user not in lineItems:
@@ -22,23 +22,9 @@ class Command(BaseCommand):
           if len(items) > 0 and subscription not in lineItems[subscription.user]['subscriptions']:
             lineItems[subscription.user]['subscriptions'].append(subscription)
             lineItems[subscription.user]['lineItems'] += items
-      for user, data in lineItems.iteritems():
-        invoice = Invoice.bundleLineItems(user, endOfMonth, data['lineItems'])
-        if invoice:
-          print "Created invoice", invoice
-        for subscription in data['subscriptions']:
-          subscription.save()
-
-  def foo(self):
-    for member in Member.objects.all():
-      invoice = member.generateMonthlyInvoice()
+    for user, data in lineItems.iteritems():
+      invoice = Invoice.bundleLineItems(user, endOfMonth, data['lineItems'])
       if invoice:
-        print "Billed", member, "for the month:"
-        for item in invoice.items.all():
-          print "\t", item
-      else:
-        print "%s has outstanding balance of $%s"%(
-          member,
-          member.outstandingBalance
-        )
-
+        print "Created invoice", invoice
+      for subscription in data['subscriptions']:
+        subscription.save()
