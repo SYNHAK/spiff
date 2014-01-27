7abfa8d52565855cfa1c55c0622b5d599cd04c2f
Torrie Fischer <tdfischer@hackerbots.net>
2014-01-27 01:06:35 -0500

    Set invoices to non-draft when done

--- spiff/subscription/management/commands/process_subscriptions.py
+++ spiff/subscription/management/commands/process_subscriptions.py
@@ -4,6 +4,7 @@ from spiff.subscription.models import SubscriptionPlan
 from spiff.api.plugins import find_api_classes
 from spiff.membership.utils import monthRange
 from spiff.membership.models import Member, RankLineItem
+import stripe
 
 class Command(BaseCommand):
   help = 'Bills active members for the month'
@@ -23,9 +24,14 @@ class Command(BaseCommand):
           if len(items) > 0 and subscription not in lineItems[subscription.user]['subscriptions']:
             lineItems[subscription.user]['subscriptions'].append(subscription)
             lineItems[subscription.user]['lineItems'] += items
+    invoices = []
     for user, data in lineItems.iteritems():
       invoice = Invoice.bundleLineItems(user, endOfMonth, data['lineItems'])
       if invoice:
         print "Created invoice", invoice
+        invoices.append(invoice)
       for subscription in data['subscriptions']:
         subscription.save()
+    for invoice in invoices:
+      invoice.draft = False
+      invoice.save()
