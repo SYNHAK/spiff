f2b4da68560a149a5c7443e40ca8f5b859accf64
Torrie Fischer <tdfischer@hackerbots.net>
2014-01-29 22:40:27 -0500

    Move all the static files into the ui/static/ directory

--- spiff/settings.py
+++ spiff/settings.py
@@ -62,7 +62,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = relpath('../static/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -70,7 +70,6 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    relpath("static/"),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -143,6 +142,7 @@ INSTALLED_APPS = (
     'spiff.sensors',
     'spiff.payment',
     'spiff.management',
+    'spiff.ui',
     'django_nose',
 )
 
