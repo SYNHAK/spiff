87eaa332e3e091c5d7c37c2cb7a80994e1291824
Torrie Fischer <tdfischer@hackerbots.net>
2014-01-30 01:05:50 -0500

    Reorganize files and add uglify processor

--- client/html/Gruntfile.js
+++ client/html/Gruntfile.js
@@ -1,12 +1,39 @@
 module.exports = function(grunt) {
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        mangle: false
+      },
+      deps: {
+        files: [{
+          expand: true,
+          cwd: 'build/lib/',
+          src: '**/*.js',
+          dest: 'app/lib'
+        }]
+      },
+      app: {
+        files: [{
+          expand: true,
+          cwd: 'app/js',
+          src: '**/*.js',
+          dest: 'app/lib',
+        }]
+      }
+    },
     bower: {
       install: {
+        options: {
+          targetDir: "build/lib",
+          cleanTargetDir: true
+        }
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-bower-task');
-  grunt.registerTask('default', ['bower']);
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.registerTask('default', ['bower', 'uglify:deps', 'uglify:app']);
 
 };
--- spiff/settings.py
+++ spiff/settings.py
@@ -70,7 +70,7 @@ STATIC_URL = '/ui/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    relpath('../client/html/'),
+    relpath('../client/html/app/'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
