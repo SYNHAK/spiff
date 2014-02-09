ebf8137e9b1dfecd1f4efaaf1c7441af3d0d8dbf
Torrie Fischer <tdfischer@hackerbots.net>
2014-02-08 21:48:47 -0500

    Wrap the whole subscription processing in a database transaction. ugly hack.

--- spiff/subscription/management/commands/process_subscriptions.py
+++ spiff/subscription/management/commands/process_subscriptions.py
@@ -1,37 +1,41 @@
 from django.core.management import BaseCommand
+from django.db import transaction
 from spiff.api.plugins import find_api_classes
 from spiff.subscription.models import SubscriptionPlan
 from spiff.payment.models import Invoice
 from spiff.membership.utils import monthRange
 from spiff.membership.models import Member, RankLineItem
+from spiff import funcLog
 import stripe
 
 class Command(BaseCommand):
   help = 'Bills active members for the month'
 
   def handle(self, *args, **options):
-    startOfMonth, endOfMonth = monthRange()
-    lineItems = {}
-    for planCls in find_api_classes('models', SubscriptionPlan):
-      plans = planCls.objects.all()
-      for plan in plans:
-        for subscription in plan.subscriptions.filter(active=True):
-          if subscription.user not in lineItems:
-            lineItems[subscription.user] = {'subscriptions': [], 'lineItems': []}
+    with transaction.atomic():
+      startOfMonth, endOfMonth = monthRange()
+      lineItems = {}
+      for planCls in find_api_classes('models', SubscriptionPlan):
+        plans = planCls.objects.all()
+        for plan in plans:
+          for subscription in plan.subscriptions.filter(active=True):
+            if subscription.user not in lineItems:
+              lineItems[subscription.user] = {'subscriptions': [], 'lineItems': []}
 
-          items = plan.process(subscription)
+            items = plan.process(subscription)
+            funcLog().info("Processed subscription %s", subscription)
 
-          if len(items) > 0 and subscription not in lineItems[subscription.user]['subscriptions']:
-            lineItems[subscription.user]['subscriptions'].append(subscription)
-            lineItems[subscription.user]['lineItems'] += items
-    invoices = []
-    for user, data in lineItems.iteritems():
-      invoice = Invoice.bundleLineItems(user, endOfMonth, data['lineItems'])
-      if invoice:
-        print "Created invoice", invoice
-        invoices.append(invoice)
-      for subscription in data['subscriptions']:
-        subscription.save()
-    for invoice in invoices:
-      invoice.draft = False
-      invoice.save()
+            if len(items) > 0 and subscription not in lineItems[subscription.user]['subscriptions']:
+              lineItems[subscription.user]['subscriptions'].append(subscription)
+              lineItems[subscription.user]['lineItems'] += items
+      invoices = []
+      for user, data in lineItems.iteritems():
+        invoice = Invoice.bundleLineItems(user, endOfMonth, data['lineItems'])
+        if invoice:
+          funcLog().info("Created invoice %s", invoice)
+          invoices.append(invoice)
+        for subscription in data['subscriptions']:
+          subscription.save()
+      for invoice in invoices:
+        invoice.draft = False
+        invoice.save()
