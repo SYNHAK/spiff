dcabe64429bfd1199bfd8f085c2fff38f2cae665
Torrie Fischer <tdfischer@hackerbots.net>
2014-02-08 21:50:29 -0500

    Implement different donation subscription workflow, with request to register

--- client/html/app/js/ui/controllers/donate.js
+++ client/html/app/js/ui/controllers/donate.js
@@ -3,12 +3,44 @@ angular.module('spiff.donate', [
   'restangular'
 ])
 
-.controller('DonateCtrl', function($scope, Restangular) {
+.controller('AddDonationSubscriptionCtrl', function($scope, Restangular, Spiff, $modalInstance, plan) {
+  $scope.plan = plan;
+  $scope.cancel = $modalInstance.close;
+  $scope.save = function() {
+    Restangular.all('subscription').post({
+      plan: '/v1/subscriptionplan/'+plan.id+'/',
+      user: '/v1/user/'+Spiff.currentUser.userid+'/',
+    }).then(function() {
+      $modalInstance.close();
+    });
+  }
+})
+
+.controller('DonateCtrl', function($scope, Restangular, Spiff, $http, $modal) {
   $scope.plans = Restangular.all('donationplan').getList().$object;
 
+  $http.get('/status.json').then(function (api) {
+    $scope.spaceAPI = api.data;
+  });
+
   $scope.startSubscription = function(plan) {
-    Restangular.all('subscription').post({
-      plan: '/v1/subscriptionplan/'+plan.id+'/'
-    });
+    if (Spiff.currentUser.isAnonymous) {
+      $modal.open({
+        templateUrl: 'donate/modal/login-needed.html',
+        controller: function($location, $scope, $modalInstance) {
+          $scope.cancel = $modalInstance.close;
+          $scope.start = function() {
+            $modalInstance.close();
+            $location.url('/register');
+          }
+        }
+      });
+    } else {
+      $modal.open({
+        templateUrl: 'donate/modal/add.html',
+        controller: 'AddDonationSubscriptionCtrl',
+        resolve: {plan: function() {return plan}}
+      })
+    }
   };
 });
