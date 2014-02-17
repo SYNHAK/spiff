bba419f3e0bbc7bf8aebc93676ba9a5fcf6ba2b9
Torrie Fischer <tdfischer@hackerbots.net>
2014-02-17 17:48:46 -0500

    Add some middleware to handle CORS

--- spiff/settings.py
+++ spiff/settings.py
@@ -101,6 +101,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'spiff.membership.middleware.AnonymousUserMiddleware',
+    'spiff.middleware.XhrMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
