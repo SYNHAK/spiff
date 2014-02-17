15aaa89b3efed136fb75e1fca953bfc8678e5aba
Torrie Fischer <tdfischer@hackerbots.net>
2014-02-17 16:23:56 -0500

    Cleanup forms and context processors

--- spiff/settings.py
+++ spiff/settings.py
@@ -169,9 +169,6 @@ TEMPLATE_CONTEXT_PROCESSORS = (
   "django.core.context_processors.static",
   "django.core.context_processors.tz",
   "django.contrib.messages.context_processors.messages",
-  'spiff.local.context.space_info',
-  'spiff.local.context.search_form',
-  'spiff.local.context.greeting_of_the_day',
 )
 
 # A sample logging configuration. The only tangible logging
