7d6a8c77f99b442f208d689c16f47b2e2424e618
Torrie Fischer <tdfischer@hackerbots.net>
2014-02-22 14:59:11 -0500

    Make resource QR codes smaller on list page

--- client/html/app/js/ui/directives.js
+++ client/html/app/js/ui/directives.js
@@ -4,15 +4,23 @@ var spiffDirectives = angular.module('spiffDirectives', [
 spiffDirectives.directive('qrCode', function($location) {
   return {
     link: function(scope, element, attrs) {
+      var size = 80;
+      if (attrs.size != undefined) {
+        size = attrs.size;
+      }
+      console.log("Making size " +size);
       if (attrs.qrCode == undefined) {
         $(element).qrcode({
           text: attrs.text,
+          width: size,
+          height: size,
           maxVersion: 4,
         });
       } else {
         $(element).qrcode({
           text: attrs.text,
-          size: 80,
+          width: size,
+          height: size,
           ecLevel: 'H',
           render: 'div'
         });
