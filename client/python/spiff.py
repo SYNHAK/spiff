9ef3e8ece2f1ba998ca675c9694439ab4bfcc5bc
Torrie Fischer <tdfischer@hackerbots.net>
2014-02-27 18:23:31 -0500

    Add new Spiff API

--- client/python/spiff.py
+++ client/python/spiff.py
@@ -1,24 +1,67 @@
 import requests
 import json
-from tastypie_client import Api
+import logging
+import datetime
 
-class API(Api):
+log = logging.getLogger('spiff')
+
+class ServerError(Exception):
+  def __init__(self, trace):
+    super(ServerError, self).__init__()
+    self.__trace = trace
+
+  def __str__(self):
+    return self.__trace
+
+class SpiffObjectEncoder(json.JSONEncoder):
+  def __init__(self, api, *args, **kwargs):
+    super(SpiffObjectEncoder, self).__init__(*args, **kwargs)
+    self.__api = api
+
+  def default(self, obj):
+    if isinstance(obj, SpiffObject):
+      return obj.resource_uri
+    if isinstance(obj, datetime.date):
+      return str(obj)
+    if isinstance(obj, ObjectList):
+      l = []
+      for o in obj:
+        l.append(o)
+      return self.default(l)
+    try:
+      iterable = iter(obj)
+    except TypeError:
+      pass
+    else:
+      return list(iterable)
+    return super(SpiffObjectEncoder, self).default(obj)
+
+class API(object):
   def __init__(self, uri, verify=True):
-    super(API, self).__init__(uri+'v1/')
+    super(API, self).__init__()
     self.__uri = uri
     self.__token = None
     self.__verify = verify
 
+  def __repr__(self):
+    return "API(%r)"%(self.__uri)
+
+  def __str__(self):
+    return repr(self)
+
   def subUri(self, uri):
     return '/'.join((self.__uri, uri))
 
-  def get(self, uri, **kwargs):
+  def getRaw(self, uri, **kwargs):
     headers = {
       'accept': 'application/json',
       'content-type': 'application/json'
     }
     if self.__token:
       headers['authorization'] = 'Bearer '+self.__token
+    log.debug("Requesting via GET %s: %r", uri, kwargs)
+    if uri[0] == '/':
+      uri = uri[1:]
     return requests.get(
       self.subUri(uri),
       verify=self.__verify,
@@ -26,8 +69,21 @@ class API(Api):
       params=kwargs
     )
 
-  def post(self, uri, value):
-    data = json.dumps(value)
+  def get(self, uri, status=200, **kwargs):
+    req = self.getRaw(uri, **kwargs)
+    if req.status_code != status:
+      if len(req.content):
+        errorMsg = req.json()
+        if 'traceback' in errorMsg:
+          raise ServerError(errorMsg['traceback'])
+        else:
+          raise ServerError(errorMsg['error'])
+      else:
+        req.raise_for_status()
+    return req.json()
+
+  def postRaw(self, uri, value):
+    data = SpiffObjectEncoder(self, indent=2).encode(value)
     headers = {
       'accept': 'application/json',
       'content-type': 'application/json'
@@ -35,6 +91,7 @@ class API(Api):
 
     if self.__token:
       headers['authorization'] = 'Bearer '+self.__token
+    log.debug("Requesting via POST %s: %s", uri, data)
     return requests.post(
       self.subUri(uri),
       data=data,
@@ -42,9 +99,21 @@ class API(Api):
       headers=headers
     )
 
+  def post(self, uri, status=200, **kwargs):
+    req = self.postRaw(uri, kwargs)
+    if req.status_code != status:
+      if len(req.content):
+        errorMsg = req.json()
+        if 'traceback' in errorMsg:
+          raise ServerError(errorMsg['traceback'])
+        else:
+          raise ServerError(errorMsg['error'])
+      else:
+        req.raise_for_status()
+    return req.json()
+
   def login(self, username, password):
-    data={'username': username, 'password': password}
-    ret = self.post('v1/member/login/', data).json()
+    ret = self.post('v1/member/login/', username=username, password=password)
     if 'token' in ret:
       self.__token = ret['token']
       return True
@@ -54,11 +123,19 @@ class API(Api):
   def uri(self):
     return self.__uri
 
+  def getOne(self, type, id=None):
+    if id is None:
+      if isinstance(type, SpiffObject):
+        return SpiffObject(self, self.get(type.resource_uri))
+      else:
+        return SpiffObject(self, self.get(type))
+    return SpiffObject(self, self.get('%s/%s'%(type, id)))
+
   def getList(self, type, **kwargs):
     return ObjectList(self, type, kwargs)
 
-  def resources(self, **kwargs):
-    return self.getList('resource', **kwargs)
+  def create(self, type, **kwargs):
+    return self.post('v1/%s/'%(type), **kwargs)
 
 class ObjectList(object):
   def __init__(self, api, type, filters={}):
@@ -83,12 +160,21 @@ class ObjectList(object):
     params.update(self.__filters)
     params['offset'] = offset
     params['count'] = count
-    data = self.__api.get('/'.join(('v1', self.__type)), **params).json()
+    data = self.__api.get('/'.join(('v1', self.__type)), **params)
     self.__max = data['meta']['total_count']
     for i in range(0, len(data['objects'])):
-      self.__cache[i+offset] = data['objects'][i]
+      self.__cache[i+offset] = SpiffObject(self.__api,
+          data['objects'][i])
 
   def __getitem__(self, offset):
+    if isinstance(offset, slice):
+      ret = []
+      step = offset.step
+      if step is None:
+        step = 1
+      for i in range(offset.start, offset.stop, step):
+        ret.append(self[i])
+      return ret
     if self.__max >= 0 and offset >= self.__max:
       raise KeyError
     if offset not in self.__cache:
@@ -97,6 +183,15 @@ class ObjectList(object):
 
   def __iter__(self):
     return ObjectListIter(self)
+
+  def __str__(self):
+    return repr(self)
+
+  def __repr__(self):
+    total = len(self)
+    if total >= 5:
+      return repr(self[0:3]+['...'])
+    return repr(self[0:len(self)])
 
 class ObjectListIter(object):
   def __init__(self, objectList):
@@ -113,12 +208,37 @@ class ObjectListIter(object):
     return self.__list[self.__pos]
 
 class SpiffObject(object):
-  def __init__(self, api, type, data):
+  def __init__(self, api, data, type=None):
     self.__api = api
-    self.__type = type
     self.__data = data
+    assert(isinstance(self.__data, dict))
+    if type is None:
+      self.__type = self.__data['resource_uri'].split('/')[1]
+    else:
+      self.__type = type
+
+  @property
+  def type(self):
+    return self.__type
 
   def update(self, **kwargs):
     for k,v in kwargs.iteritems():
       self.__data[k] = v
     self.__api.post('/'.join(('v1', self.__type)), kwargs)
+
+  def __repr__(self):
+    return "SpiffObject(%r, %r, %r)"%(self.__api, self.__data,
+        self.__type)
+
+  def __unicode__(self):
+    return str(self)
+
+  def __str__(self):
+    return "%s(%r)"%(self.__type,
+        SpiffObjectEncoder(self.__api, indent=4).encode(self.__data))
+
+  def __getattr__(self, name):
+    return self.__data[name]
+
+  def __getitem__(self, key):
+    return self.__data[key]
