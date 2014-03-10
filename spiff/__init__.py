b5c9931d75b64d11681f40a7c7badea810f8592a
Torrie Fischer <tdfischer@hackerbots.net>
2014-03-10 12:53:36 -0400

    Add a funcLog shortcut to debug

--- spiff/__init__.py
+++ spiff/__init__.py
@@ -1,11 +1,14 @@
 import logging
 import inspect
 
-def funcLog():
+def funcLog(*args, **kwargs):
   frame = inspect.stack()[1]
   localVars = frame[0].f_locals
   if 'self' in localVars:
     logName = '%s.%s.%s'%(localVars['self'].__class__.__module__, localVars['self'].__class__.__name__, frame[3])
   else:
     logName = '%s.%s'%(frame[0].f_globals['__name__'], frame[3])
-  return logging.getLogger(logName)
+  logger = logging.getLogger(logName)
+  if len(args):
+    return logger.debug(*args, **kwargs)
+  return logger
