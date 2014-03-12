75476be46b966d942eb732a70966570a34fc3d91
Torrie Fischer <tdfischer@hackerbots.net>
2014-03-12 11:34:53 -0400

    load bigger slices

--- client/python/spiff.py
+++ client/python/spiff.py
@@ -193,7 +193,7 @@ class ObjectList(object):
     self.__api = api
     self.__type = type
     self.__cache = {}
-    self.__count = 20
+    self.__count = 50
     self.__max = -1
     self.__filters = {}
     for name, value in filters.iteritems():
