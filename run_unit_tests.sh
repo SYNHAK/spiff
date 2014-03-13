f4bd6a595c094163956e8c400a61cb41ab8324d0
Torrie Fischer <tdfischer@hackerbots.net>
2014-03-12 23:14:28 -0400

    Fix missing SECRET_KEY on travis

--- run_unit_tests.sh
+++ run_unit_tests.sh
@@ -6,6 +6,9 @@ source virtualenv/bin/activate
 pip -q install -r pip-requirements
 pip -q install coverage
 coverage erase
+if [ ! -f spiff/local_settings.py ];then
+  echo "SECRET_KEY='foo'" > spiff/local_settings.py
+fi
 coverage run ./manage.py test $@
 ret=$?
 if [ $ret -eq 0 ];then
