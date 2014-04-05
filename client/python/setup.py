c1d83150779a3a32de63ea1104299a383ecc82b8
Torrie Fischer <tdfischer@hackerbots.net>
2014-04-05 01:03:02 -0400

    bump client version

--- client/python/setup.py
+++ client/python/setup.py
@@ -2,7 +2,7 @@
 
 from distutils.core import setup
 setup(name='Spiff',
-    version='0.1.4',
+    version='0.1.5',
     description="API to Spaceman Spiff",
     author='Torrie Fischer',
     author_email='tdfischer@hackerbots.net',
