9ccbe9af53e6cc6256588882795ec2177ca40d90
Torrie Fischer <tdfischer@hackerbots.net>
2014-04-06 16:30:49 -0400

    Use new bonehead-spiff integration and proper sensor value setting

--- client/bonehead/spiff/frontDoor.py
+++ client/bonehead/spiff/frontDoor.py
@@ -1,18 +1,16 @@
-import spiff
-from bonehead import Plugin
+import bonehead
 from bonehead.ui import Page
 from PyQt4 import QtCore, QtGui, QtWebKit, QtNetwork
 import threading
 
-class FrontDoorPlugin(Plugin):
+class FrontDoorPlugin(bonehead.Plugin):
     def newPage(self, name, args, ui):
         return FrontDoorPage(args['sensor-id'], ui)
 
 class FrontDoorPage(Page):
     def __init__(self, sensorID, ui):
         super(FrontDoorPage, self).__init__('Open/Close Space', ui)
-        self._api = spiff.API(ui.app.spaceAPI.raw['x-spiff-url'], verify=False)
-        self.__sensor = self._api.sensor(sensorID)
+        self.__sensor = self.spiff.getOne('sensor', sensorID)
         self.setStyleSheet("*{font-size:32pt}")
         self.layout = QtGui.QVBoxLayout(self)
         self.button = QtGui.QPushButton(self)
@@ -24,12 +22,13 @@ class FrontDoorPage(Page):
         self.updateButton()
 
     def _runToggle(self):
-        if self.__sensor.value == True:
+        if str(self.__sensor.value) == "True":
             self.text.setText("Closing space...")
-            self.__sensor.setValue(False)
+            self.__sensor.value = False
         else:
             self.text.setText("Opening space...")
-            self.__sensor.setValue(True)
+            self.__sensor.value = True
+        self.__sensor.save()
         self.updateButton()
 
     def toggle(self):
@@ -38,7 +37,7 @@ class FrontDoorPage(Page):
 
     def updateButton(self):
         self.__sensor.refresh()
-        if self.__sensor.value == True:
+        if str(self.__sensor.value) == "True":
             self.button.setStyleSheet("*{background-color: #f00;}")
             self.button.setText("Close Space")
             self.text.setText("Space is OPEN")
